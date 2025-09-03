25dbc18dc41dea9864976a5131c671ca955cb334
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-03 18:46:51 +0100

    adds buttom atom component

---
+++ src/atoms/ButtonAtom.vue
@@ -0,0 +1,30 @@
+<template>
+  <button @click="onClick" :type="type">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+withDefaults(defineProps<{ type?: "button" | "submit" | "reset" }>(), {
+  type: "button",
+});
+const emit = defineEmits<{ (e: "onClick"): void }>();
+
+function onClick() {
+  emit("onClick");
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  background-color: var(--green-600);
+  border-radius: 8px;
+  border: 1px solid var(--green-600);
+  color: var(--white);
+  font-size: 18px;
+  font-weight: bold;
+  letter-spacing: 0;
+  line-height: 150%;
+  padding: 16px 0;
+}
+</style>
