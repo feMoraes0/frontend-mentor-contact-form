a5860414d39a89f3f84bf4a0bb90f837d8b6c3a2
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-04 20:49:21 +0100

    adds label atom component

---
+++ src/atoms/LabelAtom.vue
@@ -0,0 +1,26 @@
+<template>
+  <label :for="for">
+    <slot />
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  for?: string;
+}>();
+</script>
+
+<style scoped lang="scss">
+label {
+  color: var(--grey-900);
+  font-size: 16px;
+  font-weight: 400;
+  letter-spacing: 0;
+  line-height: 150%;
+
+  &::after {
+    color: var(--green-600);
+    content: " *";
+  }
+}
+</style>
