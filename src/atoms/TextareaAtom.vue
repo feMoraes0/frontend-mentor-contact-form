06b627c74e51c36cbfdfcae02693908fbd418cd9
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-05 13:30:42 +0100

    adds textarea atom component

---
+++ src/atoms/TextareaAtom.vue
@@ -0,0 +1,53 @@
+<template>
+  <textarea
+    :id="id"
+    :name="name"
+    :required="required"
+    :onchange="onChange"
+  ></textarea>
+</template>
+
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    id: string;
+    name: string;
+    required?: boolean;
+  }>(),
+  {
+    required: false,
+  },
+);
+
+const emit = defineEmits<{
+  (e: "onChange", text: string): void;
+}>();
+
+function onChange(event: { target: { value: string } }) {
+  const { value } = event.target;
+  emit("onChange", value);
+}
+</script>
+
+<style scoped lang="scss">
+textarea {
+  border-width: 1px;
+  border-style: solid;
+  border-color: var(--grey-500);
+  background-color: var(--white);
+  border-radius: 8px;
+  padding: 12px 24px;
+  font-size: 18px;
+  line-height: 150%;
+  letter-spacing: 0;
+  font-weight: 400;
+  color: var(--grey-900);
+  cursor: pointer;
+  resize: none;
+  min-height: 295px;
+
+  &:hover {
+    border-color: var(--green-600);
+  }
+}
+</style>
