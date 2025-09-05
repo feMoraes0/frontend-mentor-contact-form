d9184924c490ec53e792bc2cd023bfdd0543b1a5
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-05 13:40:45 +0100

    adds title atom component

---
+++ src/atoms/TitleAtom.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1><slot /></h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  color: var(--grey-900);
+  font-size: 32px;
+  font-weight: bold;
+  letter-spacing: -1px;
+  line-height: 100%;
+  margin-bottom: 32px;
+}
+</style>
