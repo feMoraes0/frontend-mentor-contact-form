51f9461301e775e13f9c61415bb9702448fe57ed
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-08 18:10:12 +0100

    adds error message atom component

---
+++ src/atoms/ErrorMessageAtom.vue
@@ -0,0 +1,12 @@
+<template>
+  <p><slot /></p>
+</template>
+
+<style scoped lang="scss">
+p {
+  color: var(--error-red);
+  font-size: 16px;
+  letter-spacing: 0;
+  line-height: 150%;
+}
+</style>
