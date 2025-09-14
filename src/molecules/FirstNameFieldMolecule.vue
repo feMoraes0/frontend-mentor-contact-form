fa62db7396dccc893308133a8315b157529e4bbb
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-14 14:10:55 +0100

    adds first name field molecule component

---
+++ src/molecules/FirstNameFieldMolecule.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <LabelAtom for="first-name-input">First Name</LabelAtom>
+    <TextInputAtom
+      id="first-name-input"
+      name="first-name-input"
+      :hasError="hasError"
+      @input="onInput"
+    />
+    <ErrorMessageAtom v-if="hasError">
+      This field is required
+    </ErrorMessageAtom>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ErrorMessageAtom from "../atoms/ErrorMessageAtom.vue";
+import LabelAtom from "../atoms/LabelAtom.vue";
+import TextInputAtom from "../atoms/TextInputAtom.vue";
+
+withDefaults(defineProps<{ hasError?: boolean }>(), { hasError: false });
+const emit = defineEmits(["input"]);
+function onInput(text: string) {
+  emit("input", text);
+}
+</script>
