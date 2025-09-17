c0b62d75c9dbbcb2e529e93d54d33d976be75090
feMoraes0 <fernandomoraes.lopes@gmail.com>
2025-09-17 17:38:03 +0100

    adds last name field molecule components

---
+++ src/molecules/LastNameFieldMolecule.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <LabelAtom for="last-name-input">Last Name</LabelAtom>
+    <TextInputAtom
+      id="last-name-input"
+      name="last-name-input"
+      :hasError="hasError"
+      @input="onInput"
+    />
+    <ErrorMessageAtom v-if="hasError">
+      This field is required
+    </ErrorMessageAtom>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ErrorMessageAtom from "../atoms/ErrorMessageAtom.vue";
+import LabelAtom from "../atoms/LabelAtom.vue";
+import TextInputAtom from "../atoms/TextInputAtom.vue";
+
+withDefaults(defineProps<{ hasError?: boolean }>(), { hasError: false });
+const emit = defineEmits(["input"]);
+function onInput(text: string) {
+  emit("input", text);
+}
+</script>
