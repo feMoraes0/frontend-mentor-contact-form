b7779a39ee5b04c1ed88fd4fe2d4853178eab285
feMoraes0 <fernandomoraes.lopes@gmail.com>
2026-05-05 19:33:00 +0100

    test: unify first name, last name and email inputs as input molecule

---
+++ tests/molecules/InputMolecule.test.ts
@@ -0,0 +1,61 @@
+import { fireEvent, render, screen } from "@testing-library/vue";
+import { describe, expect, it } from "vitest";
+import InputMolecule from "../../src/molecules/InputMolecule.vue";
+
+describe("Given the Input Molecule component", () => {
+  it("When label and input name are passed, then should validate label and input properties", () => {
+    render(InputMolecule, {
+      props: {
+        label: "any_label",
+        inputName: "any-input-name",
+      },
+    });
+    const label = screen.getByText(/any_label/i);
+    const input = screen.getByRole("textbox");
+    expect(label).toBeDefined();
+    expect(input).toHaveProperty("id", "any-input-name-input");
+    expect(input).toHaveProperty("name", "any-input-name-input");
+    expect(input).toHaveProperty("type", "text");
+  });
+
+  it("When type is passed as email, then should validate label and input properties", () => {
+    render(InputMolecule, {
+      props: {
+        label: "any_label",
+        inputName: "any-input-name",
+        type: "email",
+      },
+    });
+    const label = screen.getByText(/any_label/i);
+    const input = screen.getByRole("textbox");
+    expect(label).toBeDefined();
+    expect(input).toHaveProperty("id", "any-input-name-input");
+    expect(input).toHaveProperty("name", "any-input-name-input");
+    expect(input).toHaveProperty("type", "email");
+  });
+
+  it("When error message is passed, then should validate error message component", () => {
+    render(InputMolecule, {
+      props: {
+        label: "any_label",
+        inputName: "any-input-name",
+        errorMessage: "any_error_message",
+      },
+    });
+    const errorMessageComponent = screen.queryByText(/any_error_message/i);
+    expect(errorMessageComponent).not.toBeNull();
+  });
+
+  it("When an entry is typed, then should emit input event with the inputted string", async () => {
+    const entry = "any_entry";
+    const { emitted } = render(InputMolecule, {
+      props: {
+        label: "any_label",
+        inputName: "any-input-name",
+      },
+    });
+    const input = screen.getByRole("textbox") as HTMLInputElement;
+    await fireEvent.update(input, entry);
+    expect(emitted().input).toEqual([[entry]]);
+  });
+});
