e848b588753c1ec6a1cbc0df8049d42e12671942
feMoraes0 <fernandomoraes.lopes@gmail.com>
2026-05-05 19:56:08 +0100

    test: updates and applies ConsentMolecule

--- tests/molecules/ConsentMolecule.test.ts
+++ tests/molecules/ConsentMolecule.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import ConsentMolecule from "../../src/molecules/ConsentMolecule.vue";
 
 describe("Given the Consent Molecule component", () => {
-  it("Then should validate input properties and label text content", () => {
+  it("When has error is not passed, then should validate input properties and label text content without error message", () => {
     render(ConsentMolecule);
     const input = screen.getByRole("checkbox");
     const label = screen.getByText(/i consent to being contacted by the team/i);
@@ -11,8 +11,22 @@ describe("Given the Consent Molecule component", () => {
     expect(input).toHaveProperty("name", "consent-input");
     expect(input).toHaveProperty("id", "consent-input");
     expect(input).toHaveProperty("checked", false);
-    expect(input).toHaveProperty("required", true);
     expect(label).toBeDefined();
+  });
+
+  it("When has error is passed, then should validate input properties and label text content with error message", () => {
+    render(ConsentMolecule, { props: { hasError: true } });
+    const input = screen.getByRole("checkbox");
+    const label = screen.getByText(/i consent to being contacted by the team/i);
+    const errorMessage = screen.getByText(
+      /To submit this form, please consent to being contacted/i,
+    );
+    expect(input).toHaveProperty("type", "checkbox");
+    expect(input).toHaveProperty("name", "consent-input");
+    expect(input).toHaveProperty("id", "consent-input");
+    expect(input).toHaveProperty("checked", false);
+    expect(label).toBeDefined();
+    expect(errorMessage).toBeDefined();
   });
 
   it("When label is clicked, then should checked be true and emit click event", async () => {
